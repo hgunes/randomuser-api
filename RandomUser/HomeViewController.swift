82cea4088dd25e63b130e711cbf80bfafb36eb75
harungunes <h.gunes13@gmail.com>
2022-02-15 22:47:29 +0100

    setup programmatic UI

--- RandomUser/HomeViewController.swift
+++ RandomUser/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  RandomUser
 //
 //  Created by Harun Gunes on 15/02/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    view.backgroundColor = .blue
   }
-
-
 }
-
