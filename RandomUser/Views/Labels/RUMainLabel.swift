0961422a7a11ca2389dfe3e720d472f1bbba1f08
harungunes <h.gunes13@gmail.com>
2022-02-17 20:21:29 +0100

    create custom label and initialize

---
+++ RandomUser/Views/Labels/RUMainLabel.swift
@@ -0,0 +1,28 @@
+//
+//  RUMainLabel.swift
+//  RandomUser
+//
+//  Created by Harun Gunes on 17/02/2022.
+//
+
+import UIKit
+
+class RUMainLabel: UILabel {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  private func configure() {
+    translatesAutoresizingMaskIntoConstraints = false
+    font = UIFont.boldSystemFont(ofSize: 14)
+    textColor = .systemBackground
+  }
+}
