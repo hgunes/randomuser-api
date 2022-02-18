9883b09547a9a93f779db5bf9a2ff99514f1f72f
harungunes <h.gunes13@gmail.com>
2022-02-18 20:49:05 +0100

    configure user info view and controller

--- RandomUser/Controllers/UserInfoVC.swift
+++ RandomUser/Controllers/UserInfoVC.swift
@@ -5,4 +5,77 @@
 //  Created by Harun Gunes on 18/02/2022.
 //
 
-import Foundation
+import UIKit
+
+class UserInfoVC: UIViewController {
+  
+  var user: User!
+  
+  let mainInfoView = UserInfoView(frame: .zero)
+  let otherInfoView = UIView()
+  let getLocationButton = UIButton()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    configure()
+  }
+  
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  init(user: User) {
+    super.init(nibName: nil, bundle: nil)
+    self.user = user
+  }
+  
+  
+  private func configure() {
+    view.backgroundColor = .systemBackground
+    
+    view.addSubview(mainInfoView)
+    view.addSubview(otherInfoView)
+    view.addSubview(getLocationButton)
+    
+    mainInfoView.set(user: user)
+    
+    mainInfoView.translatesAutoresizingMaskIntoConstraints = false
+    otherInfoView.translatesAutoresizingMaskIntoConstraints = false
+    getLocationButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    mainInfoView.backgroundColor = .systemGreen
+    otherInfoView.backgroundColor = .systemPurple
+    
+    getLocationButton.backgroundColor = .blue
+    getLocationButton.layer.cornerRadius = 10
+    getLocationButton.setTitle("Get location", for: .normal)
+    
+    getLocationButton.addTarget(self, action: #selector(getLocationButtonClicked), for: .touchUpInside)
+    
+    let padding: CGFloat = 20
+    
+    NSLayoutConstraint.activate([
+      mainInfoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding * 3),
+      mainInfoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+      mainInfoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+      mainInfoView.heightAnchor.constraint(equalToConstant: 220),
+      
+      otherInfoView.topAnchor.constraint(equalTo: mainInfoView.bottomAnchor, constant: padding * 2),
+      otherInfoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+      otherInfoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+      otherInfoView.heightAnchor.constraint(equalToConstant: 320),
+      
+      getLocationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+      getLocationButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+      getLocationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+      getLocationButton.heightAnchor.constraint(equalToConstant: 50)
+    ])
+  }
+  
+  @objc
+  func getLocationButtonClicked() {
+    print("Fav button clicked")
+  }
+}
--- RandomUser/Views/UserInfoView.swift
+++ RandomUser/Views/UserInfoView.swift
@@ -5,4 +5,67 @@
 //  Created by Harun Gunes on 18/02/2022.
 //
 
-import Foundation
+import UIKit
+
+class UserInfoView: UIView {
+  
+  let userAvatar = RUAvatarImageView(frame: .zero)
+  let username = RUMainLabel(align: .center, size: 32)
+  var personalDataLabel = RUMainLabel(align: .center, size: 24)
+  
+  var user: User!
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    configure()
+  }
+  
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  func set(user: User) {
+    username.text = user.name.fullName
+    personalDataLabel.text = "\(user.gender), \(user.nat)"
+    
+    NetworkManager.shared.fetchUserAvatar(for: user.picture.large) { [weak self] image in
+      guard let self = self else { return }
+      
+      DispatchQueue.main.async {
+        self.userAvatar.image = image
+      }
+    }
+  }
+  
+  
+  private func configure() {
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    addSubview(userAvatar)
+    addSubview(username)
+    addSubview(personalDataLabel)
+    
+    let avatarEdge: CGFloat = 85
+    let padding: CGFloat = 20
+    
+    NSLayoutConstraint.activate([
+      userAvatar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 25),
+      userAvatar.centerXAnchor.constraint(equalTo: centerXAnchor),
+      userAvatar.heightAnchor.constraint(equalToConstant: avatarEdge),
+      userAvatar.widthAnchor.constraint(equalToConstant: avatarEdge),
+      
+      username.topAnchor.constraint(equalTo: userAvatar.bottomAnchor, constant: 15),
+      username.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+      username.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+      username.heightAnchor.constraint(equalToConstant: 20),
+      
+      personalDataLabel.topAnchor.constraint(equalTo: username.bottomAnchor, constant: 15),
+      personalDataLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+      personalDataLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+      personalDataLabel.heightAnchor.constraint(equalToConstant: 20),
+    ])
+  }
+}
